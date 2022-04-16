5d0e4fa1d0d2e2b9c4373e6089cd371e6a7fd5cc
ABdevs29 <abhishek.elmoro@gmail.com>
2022-04-17 00:56:14 +0530

    filter tasks using categories, users, month, year, weekday, weekends

--- index.js
+++ index.js
@@ -116,9 +116,150 @@ app.delete("/tasks/:id", async (req, res) => {
   res.send(deletedTask);
 });
 
+//Filter tasks using categories
+app.get("/tasks/category/:name", async (req, res) => {
+  const client = await createConnection();
+  const category = req.params.name;
+
+  const filteredTasks = await client
+    .db("to-do-app")
+    .collection("tasks")
+    .find({ category: category })
+    .toArray();
+
+  console.log(filteredTasks);
+  res.send(filteredTasks);
+});
+
+//Filter tasks by UserName
+app.get("/tasks/user/:name", async (req, res) => {
+  const client = await createConnection();
+  const userName = req.params.name;
+
+  const filteredTasks = await client
+    .db("to-do-app")
+    .collection("tasks")
+    .find({ user_name: userName })
+    .toArray();
+
+  console.log(filteredTasks);
+  res.send(filteredTasks);
+});
+
+//Filter tasks by Year
+app.get("/tasks/year/:year", async (req, res) => {
+  const client = await createConnection();
+  const year = req.params.year;
+
+  const filteredTasks = await client
+    .db("to-do-app")
+    .collection("tasks")
+    .find({})
+    .toArray();
+
+  const result = filteredTasks.filter(
+    (el) => el.task_deadline.split("-")[2] === year
+  );
+
+  console.log(result);
+  result.length !== 0 ? res.send(result) : res.send("No results found");
+});
+
+//Filter tasks by Year and month
+app.get("/tasks/year/:year/:month", async (req, res) => {
+  const client = await createConnection();
+  const year = req.params.year;
+  const month =
+    req.params.month.split("").length === 1
+      ? `0${req.params.month}`
+      : req.params.month;
+
+  const filteredTasks = await client
+    .db("to-do-app")
+    .collection("tasks")
+    .find({})
+    .toArray();
+
+  const result = filteredTasks.filter(
+    (el) =>
+      el.task_deadline.split("-")[2] === year &&
+      el.task_deadline.split("-")[1] === month
+  );
+
+  console.log(result);
+  result.length !== 0 ? res.send(result) : res.send("No results found");
+});
+
+//Filter tasks by Weekday tasks (Mon-Fri)
+app.get("/tasks/weekday/all", async (req, res) => {
+  const client = await createConnection();
+
+  const filteredTasks = await client
+    .db("to-do-app")
+    .collection("tasks")
+    .find({})
+    .toArray();
+
+  const result = filteredTasks.filter((el) => {
+    const [day, month, year] = el.task_deadline.split("-");
+    const century = year.split("")[0] + year.split("")[1];
+    const computedYear =
+      +month > 2
+        ? +(year.split("")[2] + year.split("")[3])
+        : +(year.split("")[2] + year.split("")[3]) - 1;
+    const weekdayNum =
+      (+day +
+        Math.floor(2.6 * (+month > 2 ? +month - 2 : +month + 10) - 0.2) -
+        2 * +century +
+        +computedYear +
+        Math.floor(+computedYear / 4) +
+        Math.floor(+century / 4)) %
+      7;
+      console.log(weekdayNum);
+    if (weekdayNum !== 6 && weekdayNum !== 0) {
+      return el;
+    }
+  });
+  console.log(result);
+  result.length !== 0 ? res.send(result) : res.send("No results found");
+});
+
+//Filter tasks by Weekend tasks (Sat, Sun)
+app.get("/tasks/weekend/all", async (req, res) => {
+  const client = await createConnection();
+
+  const filteredTasks = await client
+    .db("to-do-app")
+    .collection("tasks")
+    .find({})
+    .toArray();
+
+  const result = filteredTasks.filter((el) => {
+    const [day, month, year] = el.task_deadline.split("-");
+    const century = year.split("")[0] + year.split("")[1];
+    const computedYear =
+      +month > 2
+        ? +(year.split("")[2] + year.split("")[3])
+        : +(year.split("")[2] + year.split("")[3]) - 1;
+    const weekdayNum =
+      (+day +
+        Math.floor(2.6 * (+month > 2 ? +month - 2 : +month + 10) - 0.2) -
+        2 * +century +
+        +computedYear +
+        Math.floor(+computedYear / 4) +
+        Math.floor(+century / 4)) %
+      7;
+      console.log(weekdayNum);
+    if (weekdayNum === 6 || weekdayNum === 0) {
+      return el;
+    }
+  });
+  console.log(result);
+  result.length !== 0 ? res.send(result) : res.send("No results found");
+});
+
 app.listen(PORT, () => {
   console.log("Server running at PORT: ", PORT);
 });
 
-
-//TODO: Filter tasks using categories, users, month, year, weekday, weekends.
+//TODO: Add Users database and authentication using JWT Web Tokens. Allow access to user tasks only if user is registered in database.
